335bd65753df3414bcc16ccfe189893b16a8dcf9
Nicolas Perriault <nperriault@gmail.com>
2015-10-02 17:56:15 +0200

    Updated dist files.

--- src/adapters/base.js
+++ src/adapters/base.js
@@ -10,10 +10,6 @@ export default class BaseAdapter {
     throw new Error("Not Implemented.");
   }
 
-  // batch() {
-  //   return
-  // }
-
   /**
    * Adds a record to the IndexedDB database.
    *
